3f405e797ca8257117e2eb57949e4a7b9d28edaa
LuChunYu <luchunyu2011@163.com>
2020-02-08 16:20:03 +0800

    update api response

--- backend/api/survey/controllers/survey.js
+++ backend/api/survey/controllers/survey.js
@@ -1,8 +1,20 @@
 'use strict';
-
+const { sanitizeEntity } = require('strapi-utils');
 /**
  * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async last(ctx) {
+    const count = await strapi.services.survey.count()
+    // console.log('count: ', count)
+    const params = { id: count }
+    const entity = await strapi.services.survey.findOne(params);
+    return {
+      "errno": 0,
+      "errmsg": "",
+      "data": sanitizeEntity(entity, { model: strapi.models.survey })
+    }
+  }
+};
--- backend/api/surveyresult/controllers/surveyresult.js
+++ backend/api/surveyresult/controllers/surveyresult.js
@@ -1,8 +1,23 @@
 'use strict';
-
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
 /**
  * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async create(ctx) {
+    let entity;
+    if (ctx.is('multipart')) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.surveyresult.create(data, { files });
+    } else {
+      entity = await strapi.services.surveyresult.create(ctx.request.body);
+    }
+    return {
+      "errno": 0,
+      "errmsg": ""
+    }
+    // sanitizeEntity(entity, { model: strapi.models.surveyresult });
+  },
+};
